2c7c220493332f269353c6bc41ff929b97c086f6
hynra <hynra@yahoo.com>
2022-05-17 18:44:11 +0700

    baseweb

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import App, {AppProps} from 'next/app';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider} from 'baseui';
+import {styletron} from '../styletron';
+import {SnackbarProvider,} from 'baseui/snackbar';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <React.Fragment>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Extract Number</title>
+        </Head>
+        <StyletronProvider value={styletron}>
+          <BaseProvider theme={LightTheme}>
+              <SnackbarProvider>
+                <Component {...pageProps} />
+              </SnackbarProvider>
+          </BaseProvider>
+        </StyletronProvider>
+      </React.Fragment>
+  )
+}
+
+export default MyApp
