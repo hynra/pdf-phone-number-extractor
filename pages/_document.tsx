c14830902b348da105597188c228f97cc0623b74
hynra <hynra@yahoo.com>
2022-08-16 23:04:57 +0700

    fix styletron

---
+++ pages/_document.tsx
@@ -0,0 +1,52 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext} from 'next/document'
+import {Provider as StyletronProvider} from 'styletron-react'
+import {styletron} from '../styletron'
+
+class MyDocument extends Document {
+    static async getInitialProps(context: DocumentContext) {
+        const renderPage = () =>
+            context.renderPage({
+                enhanceApp: (App) => (props) => (
+                    <StyletronProvider value={styletron}>
+                        <App {...props} />
+        </StyletronProvider>
+    ),
+    })
+
+        const initialProps = await Document.getInitialProps({
+            ...context,
+            renderPage,
+        })
+        // @ts-ignore
+        const stylesheets = styletron.getStylesheets() || []
+        return { ...initialProps, stylesheets }
+    }
+
+    render() {
+        // @ts-ignore
+        return (
+            <Html>
+                <Head>
+                    {
+                        // @ts-ignore
+                        this.props.stylesheets.map((sheet, i) => (
+                            <style
+                                className="_styletron_hydrate_"
+                        dangerouslySetInnerHTML={{ __html: sheet.css }
+                        }
+        media={sheet.attrs.media}
+        data-hydrate={sheet.attrs['data-hydrate']}
+        key={i}
+        />
+    ))}
+        </Head>
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+        </Html>
+    )
+    }
+}
+
+export default MyDocument
